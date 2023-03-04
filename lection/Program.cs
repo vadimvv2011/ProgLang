a75d8d93222e632057e660c354e2a0fdefd09f6a
Vadim Vasil'ev <vadimvv2011@gmail.com>
2023-03-04 16:04:40 +0300

    if-example added

--- lection/Program.cs
+++ lection/Program.cs
@@ -5,9 +5,34 @@
 // int number2 = new Random().Next(1,100);
 // Console.Write(number1+number2);
 
+// Console.WriteLine("Введите Ваше имя");
+// string name=Console.ReadLine();
+// if(name=="Masha")
+//     Console.Write("Привет, Маша");
+// else
+//     Console.Write("Привет");
+Console.WriteLine("Введите первое число");
+double number1=Convert.ToDouble(Console.ReadLine());
 
+Console.WriteLine ("Введите второе число");
+double number2=Convert.ToDouble(Console.ReadLine());
 
+double number3 = Convert.ToDouble(new Random().Next(1,1000));
+Console.Write("Третье число будет равно ");
+Console.WriteLine(number3);
 
+double number4=Convert.ToDouble(new Random().Next(1,1000));
+Console.Write("Четвертое число это - ");
+Console.WriteLine(number4);
 
+Console.WriteLine("Введите пятое число");
+double number5=Convert.ToDouble(Console.ReadLine());
 
+double max=number1;
+if(number2>max) max=number2;
+if(number3>max) max=number3;
+if(number4>max) max=number4;
+if(number5>max) max=number5;
 
+Console.Write("Maximum is ");
+Console.WriteLine(max);
