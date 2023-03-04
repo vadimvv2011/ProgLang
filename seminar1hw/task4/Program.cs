e568c7a2fc1aef70e1e275bd927f032933c0103d
Vadim Vasil'ev <vadimvv2011@gmail.com>
2023-03-05 02:53:03 +0300

    task6 completed

--- seminar1hw/task4/Program.cs
+++ seminar1hw/task4/Program.cs
@@ -1,5 +1,16 @@
 ﻿
 Console.WriteLine("Введите первое целое число");
 string line=Console.ReadLine();
- 
-int number1=Convert.ToInt32(string);
+int number1=Convert.ToInt32(line);
+
+Console.WriteLine("Введите второе целое число");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье целое число");
+int number3=Convert.ToInt32(Console.ReadLine());
+
+int max=number1;
+if(number2>max) max=number2;
+if(number3>max) max=number3;
+
+Console.WriteLine(max);
---
+++ seminar1hw/task6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Input one integer number");
+int number=Convert.ToInt32(Console.ReadLine());
+if(number%2==0) Console.Write("Yes");
+else Console.Write("No");
