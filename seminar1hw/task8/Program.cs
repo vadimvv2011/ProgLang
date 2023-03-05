f260fb6cd7cf562e63fe0081dd2d6ecd4f10f625
Vadim Vasil'ev <vadimvv2011@gmail.com>
2023-03-05 03:19:06 +0300

    task8 completed

---
+++ seminar1hw/task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input one number");
+int N=Convert.ToInt32(Console.ReadLine());
+int count=1;
+while (count<=N)
+{
+    if(count%2==0) 
+    {
+        Console.Write(count);
+        // if(count!=N || count!=N-1) Console.Write(", ");
+        if(count!=N)
+        {
+            if(count!=N-1) Console.Write(", ");
+        }
+    }
+    count++;
+}
