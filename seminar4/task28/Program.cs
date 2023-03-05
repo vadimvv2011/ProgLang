9ccc58e70588f318f66633504cecec974356a1b9
Vadim Vasil'ev <vadimvv2011@gmail.com>
2023-03-05 11:39:08 +0300

    seminar #4 (intro to arrays) finished

---
+++ seminar4/task28/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел 
+// от 1 до N. 4 -> 24  5 -> 120
+
+Console.WriteLine("Введите целое число");
+int N=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Proizvedenie(N));
+
+int Proizvedenie(int arg)
+{
+    int res=1;
+    for(int i=1;i<=arg; i++)
+    {
+        res=res*i;
+    }
+    return res;
+}
---
+++ seminar4/task30/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+int[] arr = new int[8];
+
+//PrintArray(ZapolnitMassiv(arr));
+
+FillArray(arr);
+PrintArray(arr);
+
+void FillArray(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i]= new Random().Next(0,2);
+    }
+}
+void PrintArray(int[] arr)
+{
+   for(int i=0;i<arr.Length;i++)
+    {
+        Console.Write(arr[i]+", ");
+    } 
+}
